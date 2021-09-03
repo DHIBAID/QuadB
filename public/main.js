b3bb1c786ac947fdeefdfefb90c1fa5c59cdf279
DHIBAID <61199043+DHIBAID@users.noreply.github.com>
2021-09-03 19:02:55 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("#table").load("tData.html"); 
+});
+
+var countdownNumberEl = document.getElementById('countdown-number');
+var countdown = 60;
+
+countdownNumberEl.textContent = countdown;
+
+setInterval(function() {
+  countdown = --countdown <= 0 ? 60 : countdown;
+
+  countdownNumberEl.textContent = countdown;
+}, 1000);
